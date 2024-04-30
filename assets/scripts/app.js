c6b5ef325ca575e4603006795b3b97c8b00686f4
Harish1651999 <hk1651999@gmail.com>
2024-04-30 07:18:47 +0530

    add variables and values.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,10 +2,14 @@
 
 // import apiKey from "./util.js";
 
-import { apiKey, abc as content } from "./util.js";
+// import { apiKey, abc as content } from "./util.js";
 
 // import * as util from "./util.js";
 
 // console.log(util.apiKey);
 
-console.log(content);
+// console.log(content);
+
+const userMessage = "Hello World";
+
+console.log(userMessage);
