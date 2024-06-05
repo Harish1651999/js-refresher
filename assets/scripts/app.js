cc9d360087102f24c03d7f193b4f351d0984256d
Harish1651999 <hk1651999@gmail.com>
2024-06-05 09:05:26 +0530

    add control structures.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -116,3 +116,21 @@
 // };
 
 // console.log(extendedUser);
+
+// Control Structures
+
+// const password = prompt("Your password");
+
+// if (password === "Hello") {
+//   console.log("Hello works");
+// } else if (password === "hello") {
+//   console.log("hello works");
+// } else {
+//   console.log("Access not granted.");
+// }
+
+const hobbies = ["Sports", "Cooking"];
+
+for (const hobby of hobbies) {
+  console.log(hobby);
+}
